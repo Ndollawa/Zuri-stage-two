f894764740fb36aeba4ad363f36429f06c867a12
ndollawa <ndollawa@yahoo.com>
2023-09-13 11:02:07 +0100

    completed task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import * as config from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
-
+const configModule = ConfigModule.forRoot({
+  isGlobal: true,
+});
 // 'mongodb://127.0.0.1:27017/Zuri'
+console.log(process.env.DATABASE_URL);
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    configModule,
     MongooseModule.forRoot(process.env.DATABASE_URL),
     UserModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import * as config from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  // const server = config.get('server');
   const port = process.env.PORT || 3500; //server.port;
 
   await app.listen(port);
